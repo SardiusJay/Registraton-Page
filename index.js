8a19c596ba1a50956ea4874e2a22abc64be5dd16
SARDIUS <71199152+SardiusJay@users.noreply.github.com>
2022-02-03 16:00:00 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+//function checkTerms() {
+//var termsCheck = document.getElementById('agree-terms');
+//var btn = document.getElementById('submit');
+//if (termsCheck.checked == true) {
+//btn.classList.remove("disabled");
+//btn.disabled = false;
+//} else {
+//btn.classList.add("disabled");
+//btn.disabled = true;
+//}
+//}
